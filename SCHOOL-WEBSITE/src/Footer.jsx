d5258ddd21957774ed864de065db4e211192efeb
Adrian-Rafael <adrianrafael494@gmail.com>
2025-03-19 12:14:27 +0800

    Created a structured

---
+++ SCHOOL-WEBSITE/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} School Website</p>
+
+        </footer>
+    );
+
+}
+export default Footer
---
+++ SCHOOL-WEBSITE/src/Names.jsx
@@ -0,0 +1,11 @@
+
+function Names() {
+    return(
+        <ul>
+            <li>Jon</li>
+            <li>James</li>
+            <li>Jeric</li>
+        </ul>
+    );
+}
+export default Names
